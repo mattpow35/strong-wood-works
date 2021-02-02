3a0a6c8d33b0fb4a2eefd96a43531f71c1cde0a1
Matt Powley <mattpow35@gmail.com>
2021-02-01 18:47:25 -0700

    added styling for nav bar

--- front-end/src/views/Products.vue
+++ front-end/src/views/Products.vue
@@ -208,12 +208,10 @@
         <p class="float-right">
           <a href="#">Back to top</a>
         </p>
-        <p>Album example is © Bootstrap, but please download and customize it for yourself!</p>
-        <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
-      </div>
+        </div>
     </footer>
 
-  
+
 </body></html>
 
 </template>
@@ -223,5 +221,19 @@
 </script>
 
 <style scoped>
+
+.navbar {
+padding: 0 1rem;
+}
+
+.navbar-nav {
+font-size: 3rem;
+padding: 10px;
+}
+
+.nav-item {
+ padding-right: 30px;
+}
+
 
 </style>
