ba2a629e0d63464c13f7c41fa43bb74740a1389b
Matt Powley <mattpow35@gmail.com>
2021-02-10 21:51:39 -0700

    unused

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -143,13 +143,13 @@
 
 <script>
 // @ is an alias to /src
-import axios from 'axios';
+//import axios from 'axios';
 
 export default {
   name: 'Home',
   data() {
     return {
-      
+
 
     }
   },
