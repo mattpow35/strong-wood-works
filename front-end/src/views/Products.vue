905365789b013298a50e641d19672c20467fe4e6
Matt Powley <mattpow35@gmail.com>
2021-02-10 21:45:46 -0700

    style

--- front-end/src/views/Products.vue
+++ front-end/src/views/Products.vue
@@ -64,7 +64,7 @@
 
             <div class="col-md-4" v-for="product in products" v-bind:key="product.id">
               <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" :src="product.path" data-holder-rendered="true">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 100%; width: 100%; display: block;" :src="product.path" data-holder-rendered="true">
                 <div class="card-body">
                   <p class="card-text">{{product.name}}</p>
                   <div class="d-flex justify-content-between align-items-center">
