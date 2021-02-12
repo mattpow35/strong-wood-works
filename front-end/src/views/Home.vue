687b8b13e598080efc142bcef387a310b2609c15
Matt Powley <mattpow35@gmail.com>
2021-02-12 10:45:35 -0700

    icon on home page

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -3,7 +3,7 @@
 
     <header>
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a style="font-size: 30px;" class="navbar-brand" href="#">Strong Wood Works</a>
+        <a style="font-size: 30px;" class="navbar-brand" href="#"><img src="../assets/icon.JPG"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
